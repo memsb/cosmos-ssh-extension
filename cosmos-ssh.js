36fdd65e5d9b06f67a41b86acd2895bf84324efc
Martin Buckley <martin.buckley@bbc.co.uk>
2016-08-26 15:36:51 +0100

    stuff

--- cosmos-ssh.js
+++ cosmos-ssh.js
@@ -1,19 +1,49 @@
-(function () {
+var MYAPP = {
 
-    var doPoll = function () {
-        $("#instances-table > tbody > tr").each(function () {
+    addCopyLinks: function () {
+        var that = this;
+        $("#instances-table > tbody > tr").each(function (i) {
             var instance = $(this);
-            var ip = instance.find("td:nth-child(3)").html();
-            var region = instance.find("td:nth-child(4)").html();
-            var link = 'ssh://' + ip + ',' + region;
+
+            var ip_td = instance.find("td:nth-child(3)")
+            var region_td = instance.find("td:nth-child(4)")
+
+            var ip = ip_td.html();
+            var region = region_td.html();
+
+            ip_td.html(that.makeButton('ip-' + i, ip));
+            region_td.html(that.makeButton('machine-' + i, ip + ',' + region));
+        });
+
+        new Clipboard('.btn');
+    },
+
+    makeButton: function (id, text) {
+        return "<button class='btn' data-clipboard-target='#" + id + "' style='padding-left: 3px; padding-right: 2px; margin-right: 0.5em;'>" +
+            "<img src='https://clipboardjs.com/assets/images/clippy.svg' alt='Copy to clipboard' style='width: 12px; height: 12px;'>" +
+            "</button>" +
+            "<span class='copy' id='" + id + "'>" + text + "</span>";
+    },
+
+    doPoll: function () {
+        var that = this;
+        $("#instances-table > tbody > tr").each(function (i) {
+            var instance = $(this);
+            var machine_address = instance.find("#machine-" + i).text();
+            var link = 'ssh://' + machine_address;
 
             if (instance.find("a i.icon-ok").length == 0) {
                 var button = instance.find("i.icon-ok");
                 button.wrap('<a href="' + link + '"></a>');
             }
         });
-        setTimeout(doPoll, 500);
-    };
+        setTimeout(function () {
+            that.doPoll()
+        }, 500);
+    }
 
-    doPoll();
-})();
+};
+
+MYAPP.addCopyLinks();
+MYAPP.doPoll();
+
